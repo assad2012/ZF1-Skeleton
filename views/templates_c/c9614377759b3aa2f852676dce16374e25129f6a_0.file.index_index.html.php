9d796bc76321e030390aa5dad55ce092d83a7fc2
assad2008 <rlk002@gmail.com>
2017-06-30 16:32:19 +0800

    更新包2017-6-30 16:32:16

--- views/templates_c/c9614377759b3aa2f852676dce16374e25129f6a_0.file.index_index.html.php
+++ views/templates_c/c9614377759b3aa2f852676dce16374e25129f6a_0.file.index_index.html.php
@@ -1,11 +1,11 @@
 <?php
-/* Smarty version 3.1.31, created on 2017-05-15 09:09:17
+/* Smarty version 3.1.31, created on 2017-06-30 14:39:47
   from "/home/data/wwwroot/ZF1-Skeleton/views/templates/index_index.html" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.31',
-  'unifunc' => 'content_5918ffbd1fbf34_62710331',
+  'unifunc' => 'content_5955f2331c71a7_65198180',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5918ffbd1fbf34_62710331 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5955f2331c71a7_65198180 (Smarty_Internal_Template $_smarty_tpl) {
 echo $_smarty_tpl->tpl_vars['hello']->value;
 }
 }
