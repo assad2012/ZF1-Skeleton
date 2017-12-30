494b7b1753853f9877108e6f434450984bbef681
assad <rlk002@gmail.com>
2017-12-30 12:20:04 +0800

    更新包2017-12-30 12:20:03

---
+++ views/templates_c/48bc79684d1dc7cf13ea6a06ec3c9d54ca1a636a_0.file.index_index.html.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.31, created on 2017-12-30 12:18:20
+  from "/www/wwwroot/zend.yeedev.xyz/views/templates/index_index.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.31',
+  'unifunc' => 'content_5a47138c745b79_07406269',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '48bc79684d1dc7cf13ea6a06ec3c9d54ca1a636a' => 
+    array (
+      0 => '/www/wwwroot/zend.yeedev.xyz/views/templates/index_index.html',
+      1 => 1514607366,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5a47138c745b79_07406269 (Smarty_Internal_Template $_smarty_tpl) {
+echo $_smarty_tpl->tpl_vars['hello']->value;
+}
+}
